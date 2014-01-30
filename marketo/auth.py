22059246bd72618fb6cc2e28f80e47f5e837a635
tomako <tom@greenplug.hu>
2014-01-30 17:12:08 +0100

    force unicode in auth header too

--- marketo/auth.py
+++ marketo/auth.py
@@ -13,8 +13,10 @@ def sign(message, encryption_key):
 def header(user_id, encryption_key):
     timestamp = rfc3339.rfc3339(datetime.datetime.now())
     signature = sign(timestamp + user_id, encryption_key)
-    return "<env:Header><ns1:AuthenticationHeader>" \
-           "<mktowsUserId>%s</mktowsUserId>" \
-           "<requestSignature>%s</requestSignature>" \
-           "<requestTimestamp>%s</requestTimestamp>" \
-           "</ns1:AuthenticationHeader></env:Header>" % (user_id, signature, timestamp)
+    return u"<env:Header><ns1:AuthenticationHeader>" \
+           u"<mktowsUserId>{user_id}</mktowsUserId>" \
+           u"<requestSignature>{signature}</requestSignature>" \
+           u"<requestTimestamp>{timestamp}</requestTimestamp>" \
+           u"</ns1:AuthenticationHeader></env:Header>".format(user_id=user_id,
+                                                              signature=signature,
+                                                              timestamp=timestamp)

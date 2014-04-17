7abdad46e692e2611836b1e40a66b424bc6b84fd
Jeff Schwaber <freyley@gmail.com>
2014-04-17 14:25:09 -0700

    fix up campaign_record.unwrap to handle the lower caseness

--- marketo/wrapper/campaign_record.py
+++ marketo/wrapper/campaign_record.py
@@ -3,13 +3,12 @@ __author__ = 'jeff'
 class CampaignRecord:
 
     def __init__(self):
-        self.attributes = {}
         self.description = None
         self.name = None
         self.id = None
 
     def __str__(self):
-        return "Lead (%s - %s)" % (self.id, self.email)
+        return "Campaign {0}".format(self.name)
 
     def __repr__(self):
         return self.__str__()
@@ -17,19 +16,9 @@ class CampaignRecord:
 
 def unwrap(xml):
     campaign = CampaignRecord()
-    campaign.id = int(xml.find('Id').text)
+    campaign.id = int(xml.find('id').text)
 
-    campaign.name = xml.find('Name').text
-    campaign.description = xml.find('Description').text
-
-    for attribute in xml.findall('.//attribute'):
-        name = attribute.find('attrName').text
-        attr_type = attribute.find('attrType').text
-        val = attribute.find('attrValue').text
-
-        if attr_type == 'integer':
-            val = int(val)
-
-        campaign.attributes[name] = val
+    campaign.name = xml.find('name').text
+    campaign.description = xml.find('description').text
 
     return campaign
